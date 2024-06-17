f11c411c45725d96cc76107310c3f19064a5f436
Sébastien Combéfis <sebastien@combefis.be>
2024-06-17 21:48:25 +0200

    Refactor server code

--- server/api/quiz.get.ts
+++ server/api/quiz.get.ts
@@ -3,44 +3,26 @@ import crypto from 'crypto'
 import Question, { IQuestion } from '@/server/models/question'
 import Quiz from '@/server/models/quiz';
 
-export default defineEventHandler(async (event) => {
-  const data = await $fetch<{
-    success: boolean
-    score: number
-  }>('https://www.google.com/recaptcha/api/siteverify', {
-    method: 'POST',
-    params: {
-      secret: process.env.GOOGLE_RECAPTCHA_SECRET_KEY,
-      response: event.node.req.headers['google-recaptcha-token'],
-    },
-  });
-
-  if (data.success && data.score > 0.5) {
-    const questions = await Question.aggregate([
-      { $sample: { size: 2 } },
-      { $project: { choices: 1, statement: 1} },
-    ])
+export default defineEventHandler(async () => {
+  const questions = await Question.aggregate([
+    { $sample: { size: 2 } },
+    { $project: { choices: 1, statement: 1} },
+  ])
     
-    const quiz = new Quiz({
-      key: null,
-      questions: questions.map((doc) => doc._id)
-    })
-    while (quiz.key === null) {
-      const key = crypto.randomUUID()
-      if (!(await Quiz.exists({ key }))) {
-        quiz.key = key
-      }
-    }
-    await quiz.save()
-
-    return {
-      key: quiz.key,
-      questions: questions.map((doc) => Question.hydrate(doc)) as IQuestion[]
+  const quiz = new Quiz({
+    key: null,
+    questions: questions.map((doc) => doc._id)
+  })
+  while (quiz.key === null) {
+    const key = crypto.randomUUID()
+    if (!(await Quiz.exists({ key }))) {
+      quiz.key = key
     }
   }
+  await quiz.save()
 
-  throw createError({
-    statusCode: 500,
-    statusMessage: 'Quiz not allowed for non human visitors!',
-  })
+  return {
+    key: quiz.key,
+    questions: questions.map((doc) => Question.hydrate(doc)) as IQuestion[]
+  }
 })
--- server/api/quiz.patch.ts
+++ server/api/quiz.patch.ts
@@ -1,34 +1,15 @@
 import Quiz from '../models/quiz';
 
 export default defineEventHandler(async (event) => {
-  const { req } = event.node
-  const data = await $fetch<{
-    success: boolean
-    score: number
-  }>('https://www.google.com/recaptcha/api/siteverify', {
-    method: 'POST',
-    params: {
-      secret: process.env.GOOGLE_RECAPTCHA_SECRET_KEY,
-      response: req.headers['google-recaptcha-token'],
-    },
-  });
+  const body = await readBody(event)
 
-  if (data.success && data.score > 0.5) {
-    const body = await readBody(event)
-
-    const quiz = await Quiz.findOne({ key: body.key })
-    if (quiz) {
-      quiz.answers = body.answers
-      quiz.ended = new Date()
+  const quiz = await Quiz.findOne({ key: body.key })
+  if (quiz) {
+    quiz.answers = body.answers
+    quiz.ended = new Date()
       
-      await quiz.save()
+    await quiz.save()
 
-      return true
-    }
+    return true
   }
-
-  throw createError({
-    statusCode: 500,
-    statusMessage: 'Quiz not allowed for non human visitors!',
-  })
 })
